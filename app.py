bee6f80679d71589948923b19523bfaa5c08891a
Jorge Ramirez <j.ramirez-hernandez@sensory-minds.com>
2020-11-08 17:48:06 +0100

    Python Server list proxy for aiven
    Python API made in falsk with 4 endpoints available
    - / return a html with the name of the project
    - /getRegions returns the available regions as a list
    - /getServers returna all the servers available
    - /getServers/<region> return all the available servers in a specific
    region

---
+++ app.py
@@ -0,0 +1,80 @@
+# app.py
+import http.client
+import json
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Example fot the default endpoint
+    """
+    return "<h1>Server Selector API</h1>"
+
+@app.route('/getRegions/', methods=['GET'])
+def getRegions():
+    """Get the list of regions available
+
+      Returns:
+          List:list of strings representing the region names
+    """
+    response = fetchAivenCloudData()
+    servers = json.loads(response)['clouds']
+
+    response = []
+
+    for server in servers:
+      if server['geo_region'] not in response:
+        response.append(server['geo_region'])
+
+    response.sort()
+
+    return jsonify(response)
+
+@app.route('/getServers/', methods=['GET'])
+def getAllServers():
+    """Get the list of all servers availables
+
+      Returns:
+          List:list of all the servers available to choose
+    """
+    response = fetchAivenCloudData()
+    servers = json.loads(response)['clouds']
+
+    return jsonify(servers)
+
+@app.route('/getServers/<region>', methods=['GET'])
+def getServersByRegion(region):
+    """Get the list of regions available
+
+      Args:
+          region: name of the region
+
+      Returns:
+          List:list of servers in a specifiv region
+    """
+    response = fetchAivenCloudData()
+    servers = json.loads(response)['clouds']
+
+    response = []
+
+    for server in servers:
+      if server['geo_region'] == region:
+        response.append(server)
+
+    return jsonify(response)
+
+def fetchAivenCloudData():
+    """ fetch the aiven cloud API """
+    conn = http.client.HTTPSConnection("api.aiven.io")
+
+    conn.request("GET", "/v1/clouds")
+
+    res = conn.getresponse()
+    data = res.read()
+
+    response = data.decode("utf-8")
+    return response
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
